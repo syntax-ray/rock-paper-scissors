dc709dd613a558e1540eb913b399bf0eb1f7548e
Donald Ray <syntaxray@gmail.com>
2025-05-25 14:18:58 +0100

    Add player scores variables

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let POSSIBLE_CHOICES = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return POSSIBLE_CHOICES[choice];
 }
 
-function getHumanChoice() {
+function getPlayerChoice() {
     let choice = prompt("Please enter your move").trim().toLowerCase();
-    return  POSSIBLE_CHOICES.includes(choice) ? choice : undefined
+    return  POSSIBLE_CHOICES.includes(choice) ? choice : undefined;
 }
