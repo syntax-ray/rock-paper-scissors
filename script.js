23be4564b5789b541ff0b6d4544b3c8095c04567
Donald Ray <syntaxray@gmail.com>
2025-05-25 14:08:19 +0100

    Add get computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello there!");
+let POSSIBLE_CHOICES = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3);
+    return POSSIBLE_CHOICES[choice];
+}
