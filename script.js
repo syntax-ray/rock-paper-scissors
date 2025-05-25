97fb3df5106008eb27f7e1241a38399c705ecae3
Donald Ray <syntaxray@gmail.com>
2025-05-25 14:29:07 +0100

    Add play 1 round logic

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ function getPlayerChoice() {
     let choice = prompt("Please enter your move").trim().toLowerCase();
     return  POSSIBLE_CHOICES.includes(choice) ? choice : undefined;
 }
+
+function playRound(playerChoice, computerChoice) {
+    switch(playerChoice) {
+        case 'rock':
+            if (computerChoice === "paper") {
+                computerScore += 1;
+            }
+            if (computerChoice === "scissors") {
+                playerScore += 1;
+            }
+            break;
+        case 'paper':
+            if (computerChoice === "rock") {
+                playerScore += 1;
+            }
+            if (computerChoice === "scissors") {
+                computerScore += 1;
+            }
+            break; 
+        case 'scissors':
+            if (computerChoice === "rock") {
+                computerScore += 1;
+            }
+            if (computerChoice === "paper") {
+                playerScore += 1;
+            }
+            break; 
+        default:
+            alert("Wrong game :)");
+    } 
+}
