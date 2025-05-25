c00fb0394f98bcb12fcd30ab29149e9aeaf07ec1
Donald Ray <syntaxray@gmail.com>
2025-05-25 14:17:30 +0100

    Add get human choice function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let POSSIBLE_CHOICES = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    choice = Math.floor(Math.random() * 3);
+    let choice = Math.floor(Math.random() * 3);
     return POSSIBLE_CHOICES[choice];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Please enter your move").trim().toLowerCase();
+    return  POSSIBLE_CHOICES.includes(choice) ? choice : undefined
 }
